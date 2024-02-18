88d90b24b03da2a7e45182ab5ea1dcfc9ca1c03a
Jonathan Christian <christianj707@gmail.com>
2024-02-18 13:11:14 -0800

    Finish project

---
+++ app/models/comment.rb
@@ -0,0 +1,6 @@
+class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :post
+
+  validates :comment, presence: true, length: { in: 5..255 }
+end
---
+++ app/models/post.rb
@@ -0,0 +1,7 @@
+class Post < ApplicationRecord
+  belongs_to :user
+  has_many :comments
+
+  validates :title, presence: true, length: { in: 5..50 }
+  validates :body, presence: true, length: { in: 25..255 }
+end
